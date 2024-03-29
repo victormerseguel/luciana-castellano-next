6b82d029ba63ae5f84df89904a511eb4af6b383d
Victor Merseguel <merseguel@gmail.com>
2024-03-29 12:36:36 +0000

    caminhos potentes fix quotes

--- src/app/(caminhos-potentes)/caminhos-potentes/components/PhraseCP.jsx
+++ src/app/(caminhos-potentes)/caminhos-potentes/components/PhraseCP.jsx
@@ -5,8 +5,8 @@ function PhraseCP() {
   return (
     <div className={styles.phraseContainer}>
       <h2>
-        "Não podemos controlar o que acontece no mundo, mas podemos preparar
-        nossas crianças para lidarem com as adversidades da vida."
+        {'"'}Não podemos controlar o que acontece no mundo, mas podemos preparar
+        nossas crianças para lidarem com as adversidades da vida.{'"'}
       </h2>
     </div>
   );
